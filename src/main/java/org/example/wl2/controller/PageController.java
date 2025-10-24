bbe43c36543ffadb4baed5afe09dc3ba606f14dd
Issa1011 <issatou1011@gmail.com>
2025-10-24 15:11:28 +0200

    login update 2

--- src/main/java/org/example/wl2/controller/PageController.java
+++ src/main/java/org/example/wl2/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
     @GetMapping("/")
     public String homePage(){
-        return "login";
+        return "home";
     }
 
   @GetMapping("/login")
